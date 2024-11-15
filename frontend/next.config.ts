230df90c0a98b4f56b7701fc8cef241d51869836
Sumit <110224690+sumituiet@users.noreply.github.com>
2024-11-15 14:33:36 +1100

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  experimental: {
-    appDir: true,
-  },
   async rewrites() {
     return [
       {
